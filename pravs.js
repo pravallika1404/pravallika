4c4583242c35c2b5ad91ca0e1adc7f81f0d27073
pravallika <36980084+pravallika1404@users.noreply.github.com>
2018-03-04 12:00:25 +0530

    Add files via upload
    new js

--- pravs.js
+++ pravs.js
@@ -13,7 +13,10 @@ getfile("pravs.json",function(text){
    let data=JSON.parse(text);
    console.log(data);
    basicinfo(data.basics);
+   eduinfo(data.education)
 });
+//flex-parent
+var main=document.querySelector('.flex-parent');
 
 function basicinfo(basic){
   var profile=document.getElementById("basics");
@@ -29,4 +32,46 @@ function basicinfo(basic){
   var photo=document.createElement("img");
   photo.src=basic.photo;
   photo.setAttribute("id","photo");
-  profile.appendChild(photo)}
+  profile.appendChild(photo);
+}
+var right=document.createElement("div");
+right.classList.add("content-child");
+//append right to main
+main.appendChild(right);
+//education div
+var e1=document.createElement("div");
+e1.classList.add("edu");
+/*appending div to right*/right.appendChild(e1);
+/*creartion of h1*/
+var h1=document.createElement("h1");
+h1.setAttribute("id","heading");
+h1.textContent=("education details");
+h1.appendChild(document.createElement("HR"));
+console.log(right);
+/*appending of h1*/e1.appendChild(h1);
+
+function eduinfo(education){
+  for(i in education){
+//course creation
+    var h2=document.createElement("h2");
+    h2.classList.add("edu1");
+    h2.textContent=education[i].course;
+/*append h2 to h1*/h1.appendChild(h2);
+    //college creation
+    var h3=document.createElement("h3");
+    h3.classList.add("edu2");
+    h3.textContent=education[i].college;
+/*append h3 to h2*/h2.appendChild(h3);
+console.log(right);
+    // for data getting
+    var ul=document.createElement("ul");
+    ul.classList.add("edu3");
+    /*append ul to h3*/h3.appendChild(ul);
+    // for(j in education.data){
+      var li=document.createElement("li");
+      li.textContent=education[i].data;
+/*append list to ul*/
+ul.appendChild(li);
+  // }
+}
+}
